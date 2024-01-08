7d12894d2df762e854e32ba28288d2038422cb7f
Abhishek <bumblebee31304@gmail.com>
2024-01-08 23:33:38 +0530

    fix: image alignment

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,15 +1,14 @@
-//homepage.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:bloc_asngment/blocs/app_blocs.dart';
 import 'package:bloc_asngment/blocs/app_events.dart';
 import 'package:bloc_asngment/blocs/app_states.dart';
 import 'package:bloc_asngment/model/app_model.dart';
 import 'package:bloc_asngment/repo/repositories.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive/hive.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   void saveImageToLocalDB(ImageModel image) async {
     await Hive.openBox('images');
@@ -38,34 +37,29 @@ class HomePage extends StatelessWidget {
             }
             if (state is ImageLoadedState) {
               List<ImageModel> imageList = state.image;
-              return ListView.builder(
-                itemCount: imageList.length,
-                itemBuilder: (_, index) {
-                  return Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4, horizontal: 8),
-                        child: Card(
-                          color: Theme.of(context).primaryColor,
-                          child: Image.network(
-                            height: MediaQuery.of(context).size.height * 0.5,
-                            imageList[index].url.toString(),
-                          ),
-                        ),
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Card(
+                      color: Theme.of(context).primaryColor,
+                      child: Image.network(
+                        imageList.first.url.toString(),
+                        height: MediaQuery.of(context).size.height * 0.5,
                       ),
-                      FloatingActionButton(
-                        onPressed: () {
-                          // Save the image to local database using Hive
-                          saveImageToLocalDB(state.image.first);
-                          // Navigate to Screen 2
-                          Navigator.pushNamed(context, '/screen2');
-                        },
-                        child: Icon(Icons.save),
-                      )
-                    ],
-                  );
-                },
+                    ),
+                    SizedBox(height: 16),
+                    FloatingActionButton(
+                      onPressed: () {
+                        // Save the image to the local database using Hive
+                        saveImageToLocalDB(state.image.first);
+                        // Navigate to Screen 2
+                        Navigator.pushNamed(context, '/screen2');
+                      },
+                      child: Icon(Icons.save),
+                    ),
+                  ],
+                ),
               );
             }
             if (state is ImageErrorState) {
